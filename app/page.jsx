9f084534b1e356f4759a0db506fbfdd6035d5b4b
Hamsini Ganesan <hamsinig123@gmail.com>
2025-09-19 15:56:29 +1000

    Changed color of text in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -9,12 +9,12 @@ export default function Home() {
 
   return (
     <div className='flex flex-col items-center justify-center min-h-full min-w-full'>
-      <SplitText text="Ready to play?" className='text-orange-300 text-[80px] font-semibold'/>
+      <SplitText text="Ready to play?" className='text-orange-400 text-[80px] font-semibold'/>
       <Link href='/gamePage'>
         <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
           <Button
           onClick={() => {}}
-          className='bg-black m-3 p-5 text-[28px] h-[70px] text-orange-300 border-solid rounded-xl border-orange-300 border-2'>Click here!
+          className='bg-black m-3 p-5 text-[28px] h-[70px] text-orange-400 border-solid rounded-xl border-orange-400 border-2'>Click here!
           </Button>
           </FadeContent>
       </Link>
