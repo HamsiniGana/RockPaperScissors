6d613ad410547bde81ec28cb5a2c1c6874764038
Hamsini Ganesan <hamsinig123@gmail.com>
2025-09-19 17:01:49 +1000

    Changing the homepage animation

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import SplitText from './components/splitText'
 import FadeContent from './components/fade'
+import AnimatedContent from './components/animatedText'
 import { Button } from '@heroui/button';
 import { Link } from "@heroui/link";
 
@@ -11,12 +12,12 @@ export default function Home() {
     <div className='flex flex-col items-center justify-center min-h-full min-w-full'>
       <SplitText text="Ready to play?" className='text-orange-400 text-[80px] font-semibold'/>
       <Link href='/gamePage'>
-        <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+        <AnimatedContent>
           <Button
           onClick={() => {}}
           className='bg-black m-3 p-5 text-[28px] h-[70px] text-orange-400 border-solid rounded-xl border-orange-400 border-2'>Click here!
           </Button>
-          </FadeContent>
+          </AnimatedContent>
       </Link>
     </div>
   );
